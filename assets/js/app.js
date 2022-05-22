12ccd33637bfa67c087f3ddc730419ef16d03df1
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-05-22 02:10:28 +0200

    a

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,17 +6,13 @@ const navbar = document.getElementById('navbar');
 let scrolled = false;
 
 window.onscroll = function () {
-  if (window.pageYOffset > 100) {
-    navbar.classList.remove('top');
-    if (!scrolled) {
-      navbar.style.transform = 'translateY(-70px)';
-    }
-    setTimeout(function () {
-      navbar.style.transform = 'translateY(0)';
-      scrolled = true;
-    }, 200);
-  } else {
-    navbar.classList.add('top');
+  if (window.pageYOffset < 100) {
+    navbar.classList.remove('transparent');
+    console.log(window.pageXOffset)
+
+  } else if (window.pageYOffset > 100)  {
+    navbar.classList.add('transparent');
+    console.log(window.pageXOffset)
     scrolled = false;
   }
 };

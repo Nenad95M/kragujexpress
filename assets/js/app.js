dc3b4cb86959ee7f5f627e3294bf78c8684e394a
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-06-05 17:05:00 +0200

    da

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,19 +2,15 @@ const year= new Date().getFullYear();
 document.getElementById('year').textContent=year;
 
 //navbar
-const navbar = document.getElementById('navbar');
-let scrolled = false;
-
+let pastScroll = window.pageYOffset;
 window.onscroll = function () {
-  if (window.pageYOffset < 100) {
-    navbar.classList.remove('transparent');
-    console.log(window.pageXOffset)
-
-  } else if (window.pageYOffset > 100)  {
-    navbar.classList.add('transparent');
-    console.log(window.pageXOffset)
-    scrolled = false;
+  let newScroll = window.pageYOffset;
+  if (pastScroll > newScroll) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-200px";
   }
+  pastScroll = newScroll;
 };
 
 const godinePoslovanja= new Date().getFullYear() -1987;

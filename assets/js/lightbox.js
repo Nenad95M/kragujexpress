795758671c0c4501fb456d9511cdb46097f9a3cc
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-06-16 00:10:01 +0200

    a

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -6,16 +6,24 @@ const images=Array.from (document.getElementById('buss-gallery').querySelectorAl
 images.forEach(image => {
 image.addEventListener('click', (e)=>{
     console.log("?")
+    //prilikom klika, dodajemo active klasu koja u css-u daje flex umesto display-none
     lightboxBg.classList.add('active');
+    //pravimo privremeni modal element img, dajemo mu sliku koju ima ona na koju smo kliknuli
     const lightboxImg=document.createElement('img');
     lightboxImg.src=image.src;
+    //dodajemo mu id gde smo uredili izgled modala u css-u
     lightboxImg.id="lightboxImg";
-    while(lightboxBg.firstChild){
-        lightboxBg.removeChild(lightboxBg.firstChild)
-    }
+   
+    //postavljamo napravljeni element kao dete
     lightboxBg.appendChild(lightboxImg);
 });
 });
+
+//uklanjam active klasu kada se klikne bilo gde na pozadinu
 lightboxBg.addEventListener('click', (e)=>{
     lightboxBg.classList.remove('active');
+ //ako prethodna slika postoji, uklanjamo je 
+ if(lightboxBg.firstChild){
+    lightboxBg.removeChild(lightboxBg.firstChild);
+}
 })

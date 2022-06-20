fd97744aece2e68b3d9738cae615a227e384c281
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-06-20 18:15:58 +0200

    a

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,31 +1,41 @@
-const mainImg=document.getElementById("mainPhoto");
-const leftArrow=document.getElementById("leftArrow");
-const rightArrow=document.getElementById("rightArrow");
-let currentPhoto=0;
-const photos=document.querySelectorAll('[data-img]');
-const imgArr=['assets/img/flota/bus2.webp',
- 'assets/img/flota/bus3.webp',
-  'assets/img/flota/bus4.webp'
-  ,'assets/img/flota/bus5.webp'];
+const mainImg = document.getElementById("mainPhoto");
+const next = document.querySelectorAll("[data-next]");
+const prev = document.querySelectorAll('[data-prev]');
+const photos = document.querySelectorAll('[data-img]');
+
+
+let currentPhoto = 0;
+
+const imgArr = ['assets/img/flota/bus2.webp',
+    'assets/img/flota/bus3.webp',
+    'assets/img/flota/bus4.webp'
+    , 'assets/img/flota/bus5.webp'];
 
 for (const img of photos) {
-    img.addEventListener('click',()=>{
-        mainImg.src=img.src;
-        })
+    img.addEventListener('click', () => {
+        mainImg.src = img.src;
+    })
 }
-leftArrow.addEventListener('click',()=>{
-    currentPhoto=currentPhoto-1;
-    if(currentPhoto<=0){
-        currentPhoto=0;
+
+prev.forEach(button => {
+   button.addEventListener('click', () => {
+    currentPhoto = currentPhoto - 1;
+    if (currentPhoto <= 0) {
+        currentPhoto = 0;
     }
-    mainImg.src=imgArr[currentPhoto];
+    mainImg.src = imgArr[currentPhoto];
 
 })
-rightArrow.addEventListener('click',()=>{
-    currentPhoto=currentPhoto+1;
-    if(currentPhoto>=3){
-        currentPhoto=3;
-    }
-    mainImg.src=imgArr[currentPhoto];
+});
 
-})
+next.forEach(button => {
+    button.addEventListener('click', () => {
+        currentPhoto = currentPhoto + 1;
+        if (currentPhoto >= 3) {
+            currentPhoto = 3;
+        }
+     mainImg.src = imgArr[currentPhoto];
+ 
+ })
+ });
+

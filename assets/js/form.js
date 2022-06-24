7769afda4b2ecf2c214672820fd0bffea83de333
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-06-24 17:33:09 +0200

    a

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,53 +1,65 @@
-(()=>{
+(() => {
 
-const forma=document.getElementById('forma');
-const notification=document.getElementById('successMessage');
+  const forma = document.getElementById('forma');
+  const notification = document.getElementById('successMessage');
+  //niz od svih korisnickih unosa
+  const requiredFormInputs = document.getElementsByClassName("required");
 
-function showMessage(res){
 
-if(res==='true'){
-  notification.classList.remove('none');
-  setTimeout(()=>{notification.classList.add('none')}, 2000);
-}
-}
+  forma.addEventListener('submit', (e) => {
 
-forma.addEventListener('submit',(e)=>{
     e.preventDefault();
 
-  const requiredFormInputs = Array.from(
-    document.getElementsByClassName("required")
-  );
-
-
-  requiredFormInputs.forEach((element) => {
-    if (element.value.trim() === "") {
-      element.placeholder = "Morate popuniti ovo polje!";
-      element.style.border="3px solid red";
+    if (formValidation()) {
+      sendMessage();
     }
-})
 
-const [fName,fEmail, fPhone,fMessage]=requiredFormInputs;
+    //iteracija kroz sve unose kako bi se proverilo da nisu prazni
+    function formValidation() {
+      for (const input of requiredFormInputs) {
+        if (input.value.trim() === "") {
+          input.placeholder = "Morate popuniti ovo polje!";
+          input.style.border = "3px solid red";
+        }
+       
+      }
+    }
 
-fetch("https://formsubmit.co/ajax/f8bafe5f11a25d2253a4a8ddae1ac614", {
-    method: "POST",
-    headers: { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    body: JSON.stringify({
-        name: fName.value,
-        email: fEmail.value,
-        subject:fPhone.value,
-        message:fMessage.value
-    })
-}
+    //Ako dobijemo odgovor sa sajta o uspesnosti slanja poruke,
+    //dodajemo a zatim uklanjamo css klase da bi se prikazala poruka u DOM-u
 
-)
-    .then(response => response.json())
-    .then(data =>
-       showMessage(data.success))
-    .catch(error => console.log(error));
+    function showMessage(res) {
+      if (res === 'true') {
+        notification.classList.remove('none');
+        setTimeout(() => { notification.classList.add('none') }, 2000);
+      }
+    }
+    //funckija koja salje poruku
 
+    function sendMessage() {
 
-})
+      const [fName, fEmail, fPhone, fMessage] = requiredFormInputs;
+
+      fetch("https://formsubmit.co/ajax/f8bafe5f11a25d2253a4a8ddae1ac614", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          name: fName.value,
+          email: fEmail.value,
+          subject: fPhone.value,
+          message: fMessage.value
+        })
+      }
+
+      )
+        .then(response => response.json())
+        .then(data =>
+          showMessage(data.success))
+        .catch(error => console.log(error));
+    }
+  }
+  )
 })();
